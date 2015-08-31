7631c0c91edabd4c083a1e46657445ebeb34a50f
YitzhakStone <YitzhakStone@users.noreply.github.com>
2015-08-31 09:31:49 -0300

    Update teste.py

--- Test/teste.py
+++ Test/teste.py
@@ -5,3 +5,5 @@ from nltk.corpus import stopwords
 texto_longo = 'o rato roeu a roupa \tdo rei de roma, \nquando ele \nestava em \'nova\' jersey.'
 
 print texto_longo.replace('\n',' ').replace('\t',' ').replace('  ', ' ')
+
+# testando integração slack-github
