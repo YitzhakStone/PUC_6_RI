7fb6ccd5b61803513537bbf81ea42155b7634c84
Yitzhak Stone <andrade@hotmail.com.br>
2015-11-01 18:42:08 -0200

    Preparando página para exemplo do instagram.

--- Test/teste.py
+++
@@ -1,9 +0,0 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-
-
-texto_longo = 'o rato roeu a roupa \tdo rei de roma, \nquando ele \nestava em \'nova\' jersey.'
-
-print texto_longo.replace('\n',' ').replace('\t',' ').replace('  ', ' ')
-
-# testando integração slack-github
